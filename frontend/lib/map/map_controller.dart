2092b939f99683f0cabd9f34e4e91070940fff37
Samuel Surovka <80292277+ssurovka@users.noreply.github.com>
2025-03-20 21:48:47 +0100

    Revert "set up base folder structure"
    This reverts commit d79c0f8acbbdb4376d08076d2b8d73739ee1cf82.

--- frontend/lib/map/map_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-/// Logic for the map view, like variables and stuff. Anything that is
-/// related to the model of the view goes here.
-class MapController with ChangeNotifier {
-  final String name = 'Svet';
-}
