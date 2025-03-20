9840cd79e3ccad96ad1e36fa1f24b10e2825a517
Samuel Surovka <80292277+ssurovka@users.noreply.github.com>
2025-03-20 18:06:18 +0100

    API setup

--- backend/backend.py
+++ backend/backend.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from cehub_api import get_current_weather
+
 
 app = FastAPI()
 
+api_key = "uab1ovjb198baaj"
 
-@app.get("/")
-def index():
-    return {"message": "Hello, World"}
+
+@app.get("/getCurrentWeather")
+def current_weather(longitude, latitude):
+    return get_current_weather(longitude, latitude)
+
+
+def call_llm_agent(longitude, latitude):
+    ...
 
 
 def serve():
