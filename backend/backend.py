27383cf2918bdf5f4fe2b628e6bcac73b9944b4f
Samuel Surovka <80292277+ssurovka@users.noreply.github.com>
2025-03-20 23:12:02 +0100

    Added CORS Middleware

--- backend/backend.py
+++ backend/backend.py
@@ -3,9 +3,23 @@ import uvicorn
 import model.llm_wrapper.services.soil_service as soil_service
 import model.llm_wrapper.services.current_weather_service as current_weather_service
 from dotenv import load_dotenv
-import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/getCurrentWeather")
